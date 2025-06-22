9600569d38ad8e00b04c485c04bf942337cd8185
RakeshSR1 <rakeshxsr7@gmail.com>
2025-06-22 21:24:04 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-import os
-
-langs = ["hiIN", "taIN", "teIN"]
-res = os.getenv("RES", "1080p")
-crunchy_domain = os.getenv("CRUNCHY_DOMAIN", "beta.crunchyroll.com")
-
-for lang in langs:
-    print(f"Starting download for {lang}")
-    os.system(f"python3 main.py --dub --sub-lang {lang} --res {res} --domain {crunchy_domain}")
